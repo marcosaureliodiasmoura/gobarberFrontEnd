2264ef49a48f59c338b01ea67d2c557c8e753e24
marcosaureliodiasmoura <marcos_moura@live.com>
2020-01-19 09:24:43 -0300

    Select Hour

--- src/pages/New/Confirmation/index.js
+++ src/pages/New/Confirmation/index.js
@@ -3,6 +3,6 @@ import {View} from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Confirmation() {
+export default function Confirmation({navigation}) {
   return <View />;
 }
